f310ad89972cff2b534f374438bed1a7f72eda1d
«Roman <research777@mail.ru>
2022-11-28 22:58:47 +0300

    DRSEMINAR5_Ex3_fnl

--- Program.cs
+++ Program.cs
@@ -22,14 +22,14 @@ void ShowArray (int [] array)
 int SumEvenNum(int[] array)
 {
     int SumEven = 0;
-    int number = 0;
+    int count = 0;
     for (int i = 0; i < array.Length; i++)
         if (array[i] % 2 == 0)
         {
             SumEven += array[i];
-            number++;
+            count++;
         }
-    return number;
+    return count;
 }
 
 Console.WriteLine("Input size if array: ");
@@ -91,21 +91,126 @@ Console.WriteLine($"The sum of odd array elements is {resultOddSum} ");
 
 
 
-//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
 //[3 7 22 2 78] -> 76
 
 // Random().NextDouble(); - генерирует значения От 0 до 1 
 // newArray[i] = new Random().Next(minValue, maxValue +1) + Random().NextDouble(); для получения double элементов
 // Math.Round(newArray[i], 3); - округление до 3-х знаков после запятой
 
-/*
-int [] CreateRandomArray (int size, int minValue, int maxValue)   //метод создания массива с рандом эл-ами
-{
-    int[] newArray = new int [size];
+//Вариант 1
 
+/*
+double [] CreateRandomArray (int size, int min, int max)   //метод создания массива с рандом эл-ами - double
+{
+    double[] newArray = new double [size];
+    
     for (int i = 0; i < size; i++)
-        newArray[i] = new Random().Next(minValue, maxValue +1) + Random().NextDouble();   // либо решить каждый в отдельной переменной и сложить результаты
-        Math.Round(newArray[i], 3);
+    {
+        newArray[i] = Math.Round (new Random().Next(min, max +1) + new Random().NextDouble(), 3);  // либо решить каждый в отдельной переменной и сложить результаты
+        
+    }
     return newArray;
 }
+
+void ShowArray (double [] array)     
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+double MinElemArray(double[] array)
+{
+    double numMin = array[0];
+
+    for(int i = 0; i < array.Length; i++)
+        if (numMin  >  array[i])
+            numMin = array[i];
+    return numMin;
+}
+
+double MaxElemArray(double[] array)
+{
+    double numMax = array[0];
+
+    for(int i = 0; i < array.Length; i++)
+        if (numMax  < array[i])
+            numMax = array[i];  
+    return numMax; 
+}
+
+Console.WriteLine("Enter your array size: ");
+int length = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine("Enter the minimum segment of your array : ");
+int minSegment = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine("Enter the maximum segment of your array : ");
+int maxSegment = Convert.ToInt32 (Console.ReadLine());
+
+double [] newArray = CreateRandomArray(length, minSegment, maxSegment);
+ShowArray(newArray);
+double MinValue = MinElemArray(newArray);
+double MaxValue = MaxElemArray(newArray);
+double diffMaxMin = MaxValue - MinValue;
+
+Console.WriteLine($"The difference between max {MaxValue} and minimum {MinValue} element of the array is {diffMaxMin}");
+*/
+
+//Вариант 2
+
+/*
+double [] CreateRandomArray (int size, int min, int max)   //метод создания массива с рандом эл-ами - double
+{
+    double[] newArray = new double [size];
+    
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i] = Math.Round (new Random().Next(min, max +1) + new Random().NextDouble(), 3);  // либо решить каждый в отдельной переменной и сложить результаты
+        
+    }
+    return newArray;
+}
+
+void ShowArray (double [] array)     
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+double diffElemArray(double[] array)
+{
+    double numMax = array[0];
+    double numMin = array[0];
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > numMax)
+            numMax = array[i];
+        if (array[i] < numMin)
+            numMin = array[i];
+    }
+    
+    return numMax - numMin; 
+}
+
+Console.WriteLine("Enter your array size: ");
+int length = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine("Enter the minimum segment of your array : ");
+int minSegment = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine("Enter the maximum segment of your array : ");
+int maxSegment = Convert.ToInt32 (Console.ReadLine());
+
+double [] newArray = CreateRandomArray(length, minSegment, maxSegment);
+
+ShowArray(newArray);
+double diffMaxMin = diffElemArray(newArray);
+
+
+Console.WriteLine($"The difference between max and having an array is {diffMaxMin} ");
 */
